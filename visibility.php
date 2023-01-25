12316bfff22e96a5dc229f33d5917250e33756b7
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-01-25 13:02:56 +0700

    Belajar visibility/acces modifier

---
+++ visibility.php
@@ -0,0 +1,101 @@
+<?php
+
+class Produk {
+
+        public $judul, $penulis, $penerbit;
+        protected $diskon=0;
+        private $harga;
+
+        public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 )
+        {
+            $this -> judul = $judul;
+            $this -> penulis = $penulis;
+            $this -> penerbit = $penerbit;
+            $this -> harga = $harga;
+        }
+
+        public function setDiskon( $diskon ) {
+            $this->diskon = $diskon;
+        }
+
+        public function getHarga() {
+            return $this->harga - ( $this->harga * $this->diskon / 100 );
+        }
+
+        public function getLabel() {
+            return "$this->judul, $this->penulis, $this->penerbit, Rp.($this->harga)";
+        }
+
+        public function getInfoProduk() {
+            $str = "{$this->getLabel()}";
+            return $str;
+        }
+}
+
+
+class Komik extends Produk {
+        public $jmlHal;
+
+        public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHal = 0 )
+        {
+            parent::__construct( $judul, $penulis, $penerbit, $harga );
+
+            $this->jmlHal = $jmlHal;
+        }
+
+        public function getInfoProduk()
+        {
+            $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHal} Halaman.";
+            return $str;
+        }
+}
+
+
+class Game extends Produk {
+        public $waktuMain;
+
+        public function __construct( $judul="judul", $penulis="penulis", $penerbit="penerbit", $harga=0, $waktuMain=0 )
+        {
+            parent::__construct( $judul, $penulis, $penerbit, $harga );
+
+            $this->waktuMain=$waktuMain;
+        }
+    
+        public function getInfoProduk() {
+            $str = "Game : " . parent::getInfoProduk() .  " ~ {$this->waktuMain} Jam.";
+            return $str;
+        }
+}
+
+
+// class CetakInfoProduk {
+
+//     public function cetak( Produk $produk ) {
+//         $str = "{$produk->getLabel()}";
+//         return $str;
+//     }
+
+// }
+
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+$produk3 = new Game("PES2017", "awikwok", "EA GAMES", 51000, 12);
+
+//Komik : Naruto | Masashi Kishimoto, Shonen Jump, 30000 - 100 halaman.
+//Game : Uncharted | Neil Druckmann, Sony Computer, 250000 - 50 jam.
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
+echo "<br>";
+echo $produk3->getInfoProduk();
+echo "<hr>";
+
+
+// visibility 
+// 1. public = bisa diakses dimana saja
+// 2. protected = bisa diakses oleh child class dari parent class
+// 3. private = hanya bisa diakses oleh class tertentu saja
+
+$produk2->setDiskon(50);
+echo $produk2->getHarga(); 
