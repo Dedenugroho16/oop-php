40b5f4eb48f5251df16c47d43b4a4c3a6a31248f
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-01-25 15:24:10 +0700

    Belajar constant, const/define

---
+++ constant.php
@@ -0,0 +1,43 @@
+<?php 
+
+
+// define('NAMA', 'Dede Nugroho');
+// echo NAMA;
+// echo "<br>";
+
+// const UMUR = 19;
+// echo UMUR;
+
+
+// class Coba {
+//     const PACAR = 'Dewi Titiyasari';
+// }
+
+// echo Coba::PACAR;
+
+
+// magic constant
+
+// echo __FILE__; 
+// hasilnya ketika dijalankan => C:\xampp\htdocs\oop-php\constant.php
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+// echo coba();
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+$obj = new Coba;
+echo $obj->kelas;
+
+
+
+
+
+
+
+
+
+?>
