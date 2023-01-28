7d38ac45351743704afa2fdb38522eac608c4995
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-01-28 21:19:36 +0700

    Teori abstract class

---
+++ abstact-class.php
@@ -0,0 +1,5 @@
+// Abstact class / kelas abstrak
+// Sebuah kelas yang tidak dapat di-instansiasi
+// Mendefinisikan interface untuk kelas lain yang menjadi turunannya
+// Berperan sebagai kerangka dasar untuk kelas turunannya
+// Harus memiliki minimal 1 method abstract di dalamnya
--- constant.php
+++ constant.php
@@ -30,7 +30,8 @@ class Coba {
     public $kelas = __CLASS__;
 }
 $obj = new Coba;
-echo $obj->kelas;
+echo $obj->kelas
+;
 
 
 
