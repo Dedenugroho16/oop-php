e346fb7da81057682b0ea8fefdda80db2918d991
Dede Nugroho <dedenugrohomm2@gmail.com>
2023-01-28 21:44:58 +0700

    Modifikasi pada kelas abstack.php

--- abstact-class.php
+++ abstact-class.php
@@ -3,3 +3,21 @@
 // Mendefinisikan interface untuk kelas lain yang menjadi turunannya
 // Berperan sebagai kerangka dasar untuk kelas turunannya
 // Harus memiliki minimal 1 method abstract di dalamnya
+// Digunakan dalam pewarisan / inhertitance untuk memaksakan implementasi method abstract yang sama untuk semua kelas turunannya
+
+// Abstract class (2)
+// Semua kelas turunan, harus mengimplementasikan method abstract yang ada di kelas abstraknya
+// Kelas abstract boleh memiliki property / method reguler 
+// Kelas abstract boleh memiliki property / static method
+
+// Contoh kelas abstract
+// class mobil extends kendaraan 
+// class laptop extends komputer 
+// class email extends komunikasi
+
+// Kenapa menggunakan kelas abstak???
+// merepresentasikan ide atau konsep dasar
+// "Composition over inheritance" Sebaiknya kita melakukan komposisi dibandingkan kita melakukan inheritance begitu saja 
+// salah satu cara untuk menerapkan Polimorphism
+// sentralisasi logic
+// mempermudah pengerjaan tim
